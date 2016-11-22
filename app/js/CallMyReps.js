ba6852c0db76fff3af47fbb8995ce881246ea4bb
Gitanshu Sardana <gitanshu11049@iiitd.ac.in>
2016-11-22 12:37:34 +0530

    Extract main component to separate js and bring android up to speed

---
+++ app/js/CallMyReps.js
@@ -0,0 +1,51 @@
+/**
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import { StyleSheet } from 'react-native';
+import Landing from './Landing';
+
+
+import { COLOR, ThemeProvider } from 'react-native-material-ui';
+
+// you can set your style right here, it'll be propagated to application
+const uiTheme = {
+    palette: {
+        primaryColor: COLOR.red900,
+    },
+    toolbar: {
+        container: {
+            height: 80,
+        },
+    },
+};
+
+export default class CallMyReps extends Component {
+  render() {
+    return (
+      <ThemeProvider uiTheme={uiTheme}>
+        <Landing />
+      </ThemeProvider>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
