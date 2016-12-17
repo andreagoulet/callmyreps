4b5155e569d654a7e3fdafd5448545c31fa4a236
Tony Gambone <tonygambone@gmail.com>
2016-12-16 23:13:31 -0500

    Do some initial setup of tests.

--- server/test/app.test.js
+++ server/test/app.test.js
@@ -2,17 +2,17 @@
 
 const assert = require('assert');
 const request = require('request');
-const app = require('../src/app');
+//const app = require('../src/app');
 
 describe('Feathers application tests', function() {
-  before(function(done) {
-    this.server = app.listen(3030);
-    this.server.once('listening', () => done());
-  });
+  //before(function(done) {
+    //this.server = app.listen(3030);
+    //this.server.once('listening', () => done());
+  //});
 
-  after(function(done) {
-    this.server.close(done);
-  });
+  //after(function(done) {
+    //this.server.close(done);
+  //});
 
   it('starts and shows the index page', function(done) {
     request('http://localhost:3030', function(err, res, body) {
--- server/test/services/scripts/index.test.js
+++ server/test/services/scripts/index.test.js
@@ -1,10 +1,49 @@
 'use strict';
 
 const assert = require('assert');
-const app = require('../../../src/app');
+const request = require('request');
 
 describe('scripts service', function() {
-  it('registered the scripts service', () => {
-    assert.ok(app.service('scripts'));
-  });
+  it('creates scripts', function(done) {
+    var baseRequest = request.defaults({
+      url: 'http://localhost:3030/scripts',
+      json: true,
+      method: 'POST'
+    });
+
+    var opts1 = {
+      body: {
+        title: 'A script to protect bunnies',
+        issue: 'Animal welfare',
+        text: 'Please think of the bunnies.'
+      }  
+    };
+
+    baseRequest.post(opts1, function(err,res,body) {
+        assert.equal(body.title, opts1.body.title);
+        assert.equal(body.issue, opts1.body.issue);
+        assert.equal(body.text, opts1.body.text);
+        assert.ok(body._id);
+        assert.ok(body.createdAt);
+        assert.ok(body.updatedAt);
+      });
+
+    var opts2 = {
+      body: {
+        title: 'A script to protect foxes',
+        issue: 'Animal welfare',
+        text: 'Please think of the foxes.'
+      } 
+    };
+    
+    baseRequest.post(opts2, function(err,res,body) {
+        assert.equal(body.title, opts2.body.title);
+        assert.equal(body.issue, opts2.body.issue);
+        assert.equal(body.text, opts2.body.text);
+        assert.ok(body._id);
+        assert.ok(body.createdAt);
+        assert.ok(body.updatedAt);
+        done();
+      });
+   });
 });
